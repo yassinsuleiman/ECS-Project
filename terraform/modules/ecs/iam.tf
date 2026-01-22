c3ae41bc2f970037574aabf3a055b48807735a3c
Yassin Suleiman <yassine.suleiman@proton.me>
2026-01-22 11:26:07 +0100

    modify: iam roles

--- terraform/modules/ecs/iam.tf
+++ terraform/modules/ecs/iam.tf
@@ -6,7 +6,13 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect    = "Allow",
-      Action    = "sts:AssumeRole",
+      Action = [
+          "sts:AssumeRole",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ],
       Principal = { Service = "ecs-tasks.amazonaws.com" }
     }]
   })
